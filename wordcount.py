afdf66bef5d5c9033c63c738a5af74d8886b0702
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-10 12:43:55 -0700

    Finished first two further study

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,6 @@
-# put your code here.
+import sys
+import string
+
 def count_words(my_file):
     poem = open(my_file)
     word_count = {}
@@ -6,8 +8,11 @@ def count_words(my_file):
         line = line.rstrip()
         words = line.split(" ")
         for word in words:
+            word = word.lower()
+            word = word.strip(string.punctuation)
             word_count[word] = word_count.get(word, 0) + 1
     for word, count in word_count.items():
         print word, count
 
-count_words("test.txt")
+print sys.argv
+count_words(my_file = sys.argv[-1])
